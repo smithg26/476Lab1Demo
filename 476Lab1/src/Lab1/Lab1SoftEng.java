e7a90aaaf1bc30a38e72e7aed52c42f0120b0611
smithg26 <smithg26@uww.edu>
2014-09-20 20:04:31 -0500

    version 1.0

--- 476Lab1/src/Lab1/Lab1SoftEng.java
+++ 476Lab1/src/Lab1/Lab1SoftEng.java
@@ -3,6 +3,6 @@ package Lab1;
 public class Lab1SoftEng {
 	public static void main(String[]args){
 		System.out.println("Lab 1 GitHub test");
-		
+		System.out.println("Testing updated version.");
 	}
 }
---
+++ 476Lab1/src/Lab1/Version2.java
@@ -0,0 +1,8 @@
+package Lab1;
+
+public class Version2 {
+	public static void main(String[] args){
+		System.out.println("Added another class for GitHub test.");
+	}
+
+}
