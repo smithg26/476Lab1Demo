500bf7385d617888aea81372d83692207eb65715
smithg26 <smithg26@uww.edu>
2014-09-22 11:38:23 -0500

    Updated Version2.0

--- 476Lab1/src/Lab1/Version2.java
+++ 476Lab1/src/Lab1/Version2.java
@@ -3,6 +3,8 @@ package Lab1;
 public class Version2 {
 	public static void main(String[] args){
 		System.out.println("Added another class for GitHub test.");
+		String x="Update";
+		System.out.print(x);
 	}
 
 }
